d258cd34ea5d5881c69b9a4f9f4adf4e8fab7f51
song <songxiangit.com>
2023-03-26 18:56:49 +0800

    'update'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,42 @@
 module.exports = {
     title: '沅芷湘兰',
     description: 'Just playing around',
-    base: '/myblob'
+    base: '.',
+    themeConfig: {
+        nav: [{
+                text: 'Hmoe',
+                link: '/'
+            },
+            {
+                text: 'CSS',
+                link: '/css/'
+            },
+            { //这个导航栏对应多个子导航栏
+                text: 'CS与浏览器', // 这里的text就是导航栏上的名字
+                items: [
+                    { text: '计算机网络', link: '/cs/network/' },
+                    { text: '浏览器相关', link: '/cs/browser/' }, // 对应的路径如下图所示
+                    { text: '数据库', link: '/cs/database/' },
+                    { text: '操作系统', link: 'cs/os/' }
+                ]
+            }
+        ],
+        sidebar: [
+            // SidebarItem
+            {
+                text: 'Foo',
+                link: '/foo/',
+                children: [
+                    // SidebarItem
+                    {
+                        text: 'github',
+                        link: 'https://github.com',
+                        children: [],
+                    },
+                ],
+            },
+        ],
+    },
+
+
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,17 @@
-# 给兰兰看看
+---
+home: true
+# heroImage: https://img1.baidu.com/it/u=413643897,2296924942&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500
+heroImage: img/img.jpg
+heroText: 前端开发
+tagline: 记录下平时的一些前端技术
+actions:
+  - text: 快速上手
+features:
+  - title: 简洁至上
+    details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+  - title: Vue 驱动
+    details: 享受 Vue 的开发体验，可以在 Markdown 中使用 Vue 组件，又可以使用 Vue 来开发自定义主题。
+  - title: 高性能
+    details: VuePress 会为每个页面预渲染生成静态的 HTML，同时，每个页面被加载的时候，将作为 SPA 运行。
+footer: 个人博客 测试使用
+---
