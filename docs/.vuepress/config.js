ab3af5205c88f4673f9d7d8087e700b72d10b86d
song <songxiangit.com>
2023-03-27 22:40:04 +0800

    'update'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Just playing around',
     head: [
         ['link', { rel: 'icon', href: '/img/img.jpg' }]
-      ],
+    ],
     base: '/myblob/',
     themeConfig: {
         nav: [{
@@ -18,7 +18,7 @@ module.exports = {
                 text: 'js', // 这里的text就是导航栏上的名字
                 items: [
                     { text: 'vue2', link: '/js/vue2/' },
-                    { text: 'vue3', link: '/js/vue3/' }, 
+                    { text: 'vue3', link: '/js/vue3/' },
                     { text: 'react', link: '/js/react/' },
                     { text: '数据库', link: '/js/database/' },
                 ]
@@ -27,18 +27,18 @@ module.exports = {
                 text: '数据可视化', // 这里的text就是导航栏上的名字
                 items: [
                     { text: 'gos', link: '/chat/gojs/' },
-                    { text: 'antVg6', link: '/chat/antvg6/' }, 
+                    { text: 'antVg6', link: '/chat/antvg6/' },
                     { text: 'D3', link: '/chat/d3/' },
                     { text: 'echarts', link: '/chat/echarts/' },
                 ]
             },
             {
-                text:'GitHub',
-                link:'https://github.com/newbiesx/myblob'
+                text: 'GitHub',
+                link: 'https://github.com/newbiesx/myblob'
             }
         ],
         sidebar: {
-            "/css/":["cssBase"]
+            "/css/": ["cssBase"]
         },
     },
 
